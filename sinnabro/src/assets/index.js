7df812b26b1142d18f365e93895021c8880eb89d
SEOg-u <seogyeoul1898@gmail.com>
2022-12-04 16:27:52 +0900

    Create Assets Index

---
+++ sinnabro/src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as imgLogo } from "./img/IMGLogo.png";
+export { default as xBt } from "./img/XBt.png";
+export { default as bell } from "./img/Bell.png";
+export { default as pencil } from "./img/Pencil.png";
