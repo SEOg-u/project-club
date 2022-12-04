640cff4d011682c7cb32ad1369dda99fd383be9d
SEOg-u <seogyeoul1898@gmail.com>
2022-12-04 16:26:03 +0900

    Create Project Settings

---
+++ sinnabro/src/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
